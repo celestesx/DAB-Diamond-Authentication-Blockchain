3527eb636f71c25df84529c171e9d8ec3788a507
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-13 19:07:37 +1000

    Add register entity function with checks for lowercase and uppercase according to role

--- contracts/EntityContract.sol
+++ contracts/EntityContract.sol
@@ -14,4 +14,67 @@ contract EntityContract {
         string licenseNumber;
         string role;
     }
+
+    //map entitiy addresses to their info
+    mapping(address => Entity) private entities;
+
+    //registered entity addresses by role
+    mapping(string => address[]) private entitiesByRole;
+
+    //events
+    event EntityRegistered(address indexed entityAddress, string name, string role, string licenseNumber);
+
+    //function to register entity
+    function registerEntity(
+        string memory _name,
+        string memory _location,
+        string memory _role,
+        string memory _licenseNumber
+    ) public {
+        string memory normalisedRole = normaliseRole(_role);
+        
+        require(
+            isValidRole(normalisedRole),
+            "Invalid role. Must be Miner, Manufacturer, Certifier, or Retailer"
+        );
+    }
+
+    function normaliseRole(string memory _role) private pure returns (string memory) {
+        bytes32 roleHash = keccak256(abi.encodePacked(_toLowerCase(_role)));
+        
+        if (roleHash == keccak256(abi.encodePacked(_toLowerCase(ROLE_MINER)))) {
+            return ROLE_MINER;
+        } else if (roleHash == keccak256(abi.encodePacked(_toLowerCase(ROLE_MANUFACTURER)))) {
+            return ROLE_MANUFACTURER;
+        } else if (roleHash == keccak256(abi.encodePacked(_toLowerCase(ROLE_CERTIFIER)))) {
+            return ROLE_CERTIFIER;
+        } else if (roleHash == keccak256(abi.encodePacked(_toLowerCase(ROLE_RETAILER)))) {
+            return ROLE_RETAILER;
+        } else {
+            return _role;
+        }
+    }
+
+        function _toLowerCase(string memory _str) private pure returns (string memory) {
+        bytes memory bStr = bytes(_str);
+        bytes memory bLower = new bytes(bStr.length);
+        
+        for (uint i = 0; i < bStr.length; i++) {
+            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
+                bLower[i] = bytes1(uint8(bStr[i]) + 32);
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
+        
+        return string(bLower);
+    }
+
+        function isValidRole(string memory _role) private pure returns (bool) {
+        return keccak256(abi.encodePacked(_role)) == keccak256(abi.encodePacked(ROLE_MINER)) ||
+               keccak256(abi.encodePacked(_role)) == keccak256(abi.encodePacked(ROLE_MANUFACTURER)) ||
+               keccak256(abi.encodePacked(_role)) == keccak256(abi.encodePacked(ROLE_CERTIFIER)) ||
+               keccak256(abi.encodePacked(_role)) == keccak256(abi.encodePacked(ROLE_RETAILER));
+    }
+
 }
