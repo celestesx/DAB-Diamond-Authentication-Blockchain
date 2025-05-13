f98e003d935d9e58e11773bcda67f23a941285a5
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-14 09:53:25 +1000

    add provenance function bones

--- contracts/ProvenanceContract.sol
+++ contracts/ProvenanceContract.sol
@@ -20,6 +20,17 @@ contract ProvenanceContract {
         uint256 rawDiamondID; //this is for the original raw diamond
     }
 
-    
+    mapping(uint256 => Diamond) public diamonds;
+    uint256 private nextDiamondID = 1;
 
+    constructor(address _entityContractAddress) {
+        entityContract = EntityContract(_entityContractAddress);
+    }
+
+    //to implement
+    // 1. registerRawDiamond - for miners to register new diamonds
+    // 2. transferDiamond - transfer diamond between stakeholders
+    // 3. processDiamond - manufacturers process raw diamonds into new ones
+    // 4. certifyDiamond - certifiers add certification details
+    // 5. getDiamondHistory - get complete history of a diamond
 }
