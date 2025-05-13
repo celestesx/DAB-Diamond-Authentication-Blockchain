6401b820197c524fa89a314cd51082d935efec58
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-14 09:32:17 +1000

    Create provenance and marketplace contract bones

---
+++ contracts/MarketplaceContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./EntityContract.sol";
+import "./ProvenanceContract.sol";
+
+contract MarketplaceContract {
+    EntityContract private entityContract;
+    ProvenanceContract private provenanceContract;
+}
