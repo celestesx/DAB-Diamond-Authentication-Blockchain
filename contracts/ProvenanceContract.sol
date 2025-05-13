6fbc18889d660d6490edbbe09e6d4f96cccfcbe1
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-14 09:46:24 +1000

    add diamond struct

--- contracts/ProvenanceContract.sol
+++ contracts/ProvenanceContract.sol
@@ -5,5 +5,21 @@ import "./EntityContract.sol";
 
 contract ProvenanceContract {
     EntityContract private entityContract;
+
+    //diamond struct to track provenance
+    struct Diamond {
+        uint256 id;
+        address currentOwner;
+        string origin;
+        uint256 extractionDate;
+        uint256 weight;
+        string characteristics;
+        bool isCertified;
+        string certificatioID;
+        string[] processingHistory;
+        uint256 rawDiamondID; //this is for the original raw diamond
+    }
+
     
+
 }
