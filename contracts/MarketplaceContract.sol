0eb6c73ef63a4ac65fb489ef2a6bfdd7e09ee57e
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-14 10:36:59 +1000

    add marketplace contract bones

--- contracts/MarketplaceContract.sol
+++ contracts/MarketplaceContract.sol
@@ -18,5 +18,15 @@ contract MarketplaceContract {
     mapping(uint256 => DiamondStatus) public diamondStatuses;
     mapping(uint256 => address) public consumerOwners;
 
-    
+    constructor(address _entityContractAddress, address _provenanceContractAddress) {
+        entityContract = EntityContract(_entityContractAddress);
+        provenanceContract = ProvenanceContract(_provenanceContractAddress);
+    }
+
+    //to be implemented
+    // 1. listDiamondForSale - retailers list diamonds for consumer purchase
+    // 2. purchaseDiamond - consumers buy from retailers
+    // 3. transferOwnership - consumer-to-consumer transfers
+    // 4. reportStolen - mark diamonds as stolen
+    // 5. generateProvenance - create consumer-facing provenance certificate
 }
