c360de2fd98f3d085577a547ca50c7afbdddb332
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-14 10:26:14 +1000

    Update MarketplaceContract.sol

--- contracts/MarketplaceContract.sol
+++ contracts/MarketplaceContract.sol
@@ -7,4 +7,16 @@ import "./ProvenanceContract.sol";
 contract MarketplaceContract {
     EntityContract private entityContract;
     ProvenanceContract private provenanceContract;
+
+    struct DiamondStatus {
+        bool isForSale;
+        uint256 price;
+        bool isStolenMissing;
+        string statusDetails;
+    }
+
+    mapping(uint256 => DiamondStatus) public diamondStatuses;
+    mapping(uint256 => address) public consumerOwners;
+
+    
 }
