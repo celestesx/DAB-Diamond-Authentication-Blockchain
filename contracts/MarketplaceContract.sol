34e5d61467ebc672cb42cfea3986b52d54f1630e
Liane Wong <89564832+celestesx@users.noreply.github.com>
2025-05-16 11:56:09 +1000

    Update MarketplaceContract.sol

--- contracts/MarketplaceContract.sol
+++ contracts/MarketplaceContract.sol
@@ -9,7 +9,7 @@ contract MarketplaceContract is Ownable{
     EntityContract private entityContract;
     ProvenanceContract private provenanceContract;
 
-    enum DiamondStatus {Avaialable, Sold, Stolen, Lost}
+    enum DiamondStatus {Available, Sold, Stolen, Lost}
 
     struct DiamondListing {
         uint256 diamondID;
@@ -31,14 +31,58 @@ contract MarketplaceContract is Ownable{
         bool isResolved;
     }
 
+    ////////////mappings
     mapping(uint256 => DiamondListing) private diamondListings;
     mapping(uint256 => OwnershipRecord) private consumerOwnership;
     mapping(uint256 => TheftReport[]) private theftReports;
+    ////////////
 
+    ///////////events
+    event DiamondListed(uint256 indexed diamondId, address indexed seller);
+    ///////////
+
+    //constructor
     constructor(address _entityContractAddress, address _provenanceContractAddress) 
         Ownable(msg.sender) 
     {
         entityContract = EntityContract(_entityContractAddress);
         provenanceContract = ProvenanceContract(_provenanceContractAddress);
     }
+
+    //function to list the diamond by retailer
+    function listDiamond(uint256 _diamondID) external {
+        //check if the caller is the diamond owner in the provenance contract
+        (uint256 id, address currentOwner, , , , , , , ) = provenanceContract.getDiamondInfo(_diamondID);
+        require(currentOwner == msg.sender, "Only the diamond owner can list it");
+        
+        //check if the caller is a registered retailer
+        (,,bool isRegistered,,string memory role) = entityContract.getEntityInfo(msg.sender);
+        require(isRegistered, "Caller is not a registered entity");
+        require(keccak256(abi.encodePacked(role)) == keccak256(abi.encodePacked("Retailer")),
+            "Only retailers can list diamonds for initial sale");
+        
+        //Create the listing
+        diamondListings[_diamondID] = DiamondListing({
+            diamondID: _diamondID,
+            seller: msg.sender,
+            isListed: true,
+            status: DiamondStatus.Available
+        });
+        
+        emit DiamondListed(_diamondID, msg.sender);
+    }
+
 }
+
+//TO IMPLEMENT
+//DONE listDiamond
+//sellToConsumer
+//transferConsumerDiamond
+//reportStolen
+//reportLost
+//resolveTheftReport
+//verifyDiamond
+//getDiamondStatus
+//getConsumerOwnership
+//generateProvenanceCertificate
+//
